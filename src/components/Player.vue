01d4e4f0819cacc6896fc1d219e8c1fbcc48fd6f
shotaro kaneda <juanpablolvl99@gmail.com>
2020-04-15 19:12:25 -0300

    Adding more player funcionalities, seek song position, and the slide now goes with the song time.

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -2,22 +2,73 @@
     <div id="player" class="row">
         <div id="song-preview" class="col s2">
             <div>
-                <img v-if="currentTrackMetadata.image" :src="currentTrackMetadata.image">            
+                <img v-if="currentTrack.image" :src="currentTrack.image">            
             </div>
             <div id="song-info">
-                <p id="song-name">{{ currentTrackMetadata.name }}</p>
-                <p id="artist-name">{{ currentTrackMetadata.artist }}</p>
+                <p id="song-name">{{ currentTrack.name }}</p>
+                <p id="artist-name">{{ currentTrack.artist }}</p>
             </div>
         </div>
-        <div id="sdk-player" class="col s10">
+        <div id="sdk-player" class="col s8">
             <div id="options">
+                <i class="material-icons left waves-wispy minorOp" 
+                    @click="shuffle"
+                    :style="{color: isShuffled ? 'blue': '#A8C0D8'}" 
+                    id="shuffle"
+                >
+                    shuffle
+                </i>
                 <i class="material-icons left waves-effect waves-wispy" @click="skipToPrevious">skip_previous</i>
                 <i v-if="isPlaying" class="material-icons left waves-effect waves-wispy" @click="pause">pause</i>
                 <i v-else class="material-icons left waves-effect waves-wispy" @click="resume">play_arrow</i>
                 <i class="material-icons left waves-effect waves-wispy" @click="skipToNext">skip_next</i>
+                <i class="material-icons left waves-wispy minorOp"
+                    @click="repeat"
+                    :style="{color: isRepating ? 'blue': '#A8C0D8'}"
+                    id="repeat"
+                >
+                    {{ currentRepeatMode === 'track' ? 'repeat_one' : 'repeat' }}
+                </i>
             </div>
-            <div id="slide">
-                <VueSlider></VueSlider>
+            <div style="display: flex; flex-diretion: row; margin-left: 17%;">
+                <div 
+                    :style="[progress ? {} : {color: '#ededed'}, {marginTop: '0.3%'}]"
+                >
+                    {{ getTimeDisplay(progress) }}
+                </div>
+                <div id="slide">
+                    <VueSlider 
+                        v-model="progress"
+                        :dot-size="15"
+                        :silent="true"
+                        :process-style="{ background: '#A8C0D8' }"
+                        :bg-style="{ background: '#737575' }"
+                        :lazy="true" 
+                        :max="currentTrack.duration ? currentTrack.duration : null"
+                        v-on:drag-end="seekPosition">
+                    </VueSlider>
+                </div>
+                <div style="margin-top: 0.3%;">
+                    {{ currentTrack.duration ? getTimeDisplay(currentTrack.duration) : null }}
+                </div>
+            </div>
+        </div>
+        <div class="col s2" id="volumeAndDevice">
+            <i class="material-icons left waves-wispy deviceIcon">devices</i>
+            <i v-if="volume" class="material-icons left waves-wispy volumeIcon">volume_up</i>
+            <i v-else class="material-icons left waves-wispy volumeIcon">volume_off</i>
+            <div style="width: 50%;">
+                <VueSlider 
+                    v-model="volume"
+                    tooltip="none"
+                    :dot-size="15"
+                    :silent="true"
+                    :process-style="{ background: '#A8C0D8' }"
+                    :bg-style="{ background: '#737575' }"
+                    :lazy="true" 
+                    :max="100"
+                    v-on:drag-end="changeVolume">
+                </VueSlider>
             </div>
         </div>
     </div>
@@ -27,6 +78,7 @@
 import VueSlider from 'vue-slider-component';
 import 'vue-slider-component/theme/default.css';
 import axios from 'axios';
+import moment from 'moment';
 import props from '../props';
 
 export default {
@@ -38,51 +90,73 @@ export default {
             isPlaying: false,
             player: {},
             deviceId: null,
-            currentTrackMetadata: {},
-            // isShuffled: false,
+            currentTrack: {},
+            progress: 0,
+            volume: 0,
+            isShuffled: false,
+            isRepating: false,
+            //
+            timerId: null,
+            repeatModes: ['off', 'context', 'track',],
+            currentRepeatMode: null,
         }
     },
     mounted() {
-        const token = JSON.parse(window.localStorage.getItem('spotify')).access_token;
-        this.player = new window.Spotify.Player({
-            name: 'Web Playback SDK Quick Start Player',
-            getOAuthToken: cb => { cb(token); }
-        });
+        // const token = JSON.parse(window.localStorage.getItem('spotify')).access_token;
+        // this.player = new window.Spotify.Player({
+        //     name: 'Web Playback SDK Quick Start Player',
+        //     getOAuthToken: cb => { cb(token); }
+        // });
 
-        // Error handling
-        this.player.addListener('initialization_error', ({ message }) => { console.error(message); });
-        this.player.addListener('authentication_error', ({ message }) => { console.error(message); });
-        this.player.addListener('account_error', ({ message }) => { console.error(message); });
-        this.player.addListener('playback_error', ({ message }) => { console.error(message); });
+        // // Error handling
+        // this.player.addListener('initialization_error', ({ message }) => { console.error(message); });
+        // this.player.addListener('authentication_error', ({ message }) => { console.error(message); });
+        // this.player.addListener('account_error', ({ message }) => { console.error(message); });
+        // this.player.addListener('playback_error', ({ message }) => { console.error(message); });
 
-        // Playback status updates
-        this.player.addListener('player_state_changed', state => { 
-            //
-            this.isPlaying = !state.paused;
-            // this.isShuffled = state.shuffle;
-            //
-            const currentTrack = state.track_window.current_track;
-            this.currentTrackMetadata.image = currentTrack.album.images[1].url;
-            this.currentTrackMetadata.name = currentTrack.name;
-            this.currentTrackMetadata.artist = currentTrack.artists[0].name;
-            //
-            console.log(state);
-            console.log(this.currentTrackMetadata);
-        });
+        // // Playback status updates
+        // this.player.addListener('player_state_changed', state => { 
+        //     // clear time positions
+        //     this.progress = 0;
+        //     //
+        //     this.isPlaying = !state.paused;
+        //     // this.isShuffled = state.shuffle;
+        //     //
+        //     const currentTrack = state.track_window.current_track;
+        //     this.currentTrack.image = currentTrack.album.images[1].url;
+        //     this.currentTrack.name = currentTrack.name;
+        //     this.currentTrack.artist = currentTrack.artists[0].name;
+        //     this.currentTrack.duration = currentTrack.duration_ms;
+        //     //
+        //     this.progress = state.position;
+        //     //
+        //     clearInterval(this.timerId);
+        //     this.timerId = setInterval(() => {
+        //         if (!this.isPlaying) {
+        //             return ;
+        //         }
+        //         if (this.progress + 1000 < this.currentTrack.duration) {
+        //             this.progress += 1000;
+        //         }
+        //         else {
+        //             clearInterval(this.timerId);
+        //         }
+        //     }, 1000);
+        // });
 
-        // Ready
-        this.player.addListener('ready', ({ device_id }) => {
-            console.log('Ready with Device ID', device_id);
-            this.deviceId = device_id;
-        });
+        // // Ready
+        // this.player.addListener('ready', ({ device_id }) => {
+        //     console.log('Ready with Device ID', device_id);
+        //     this.deviceId = device_id;
+        // });
 
-        // Not Ready
-        this.player.addListener('not_ready', ({ device_id }) => {
-            console.log('Device ID has gone offline', device_id);
-        });
+        // // Not Ready
+        // this.player.addListener('not_ready', ({ device_id }) => {
+        //     console.log('Device ID has gone offline', device_id);
+        // });
 
-        // Connect to the player!
-        this.player.connect();
+        // // Connect to the player!
+        // this.player.connect();
     },
     methods: {
         pause() {
@@ -113,6 +187,57 @@ export default {
                 },
             });
         },
+        seekPosition() {
+            axios.put(`${props.api}/me/player/seek?device_id=${this.deviceId}&position_ms=${this.progress}`, 
+                null, 
+                {
+                    headers: {
+                        Authorization: 'Bearer ' + JSON.parse(window.localStorage.getItem('spotify')).access_token,
+                    },
+                },
+            );
+        },
+        shuffle() {
+            axios.put(`${props.api}/me/player/shuffle?device_id=${this.deviceId}&state=${!this.isShuffled}`,
+                null,
+                {
+                    headers: {
+                        Authorization: 'Bearer ' + JSON.parse(window.localStorage.getItem('spotify')).access_token,
+                    },
+                },
+            );
+        },
+        repeat() {
+            let nextRepeatModeIndex = this.repeatModes.indexOf(this.currentRepeatMode) + 1;
+            axios.put(`${props.api}/me/player/repeat?device_id=${this.deviceId}&state=${this.repeatModes[nextRepeatModeIndex]}`,
+                null,
+                {
+                    headers: {
+                        Authorization: 'Bearer ' + JSON.parse(window.localStorage.getItem('spotify')).access_token,
+                    },
+                },
+            );
+        },
+        changeVolume() {
+
+        },
+        getTimeDisplay(ms) {
+            var seconds = (ms / 1000).toFixed(0);
+            var minutes = Math.floor(seconds / 60);
+            var hours = "";
+            if (minutes > 59) {
+                hours = Math.floor(minutes / 60);
+                hours = (hours >= 10) ? hours : "0" + hours;
+                minutes = Math.floor(minutes % 60);
+                minutes = (minutes >= 10) ? minutes : "0" + minutes;
+            }
+            seconds = Math.floor(seconds % 60);
+            seconds = (seconds >= 10) ? seconds : "0" + seconds;
+            if (hours != "") {
+                return hours + ":" + minutes + ":" + seconds;
+            }
+            return "0" +  minutes + ":" + seconds;
+        },
     },
 }
 </script>
@@ -141,33 +266,44 @@ export default {
     bottom: 0;
     position: absolute;
     margin: 0;
-    /* padding: 0.3% 0% 0.3% 0%; */
+    padding: 0.2% 0% 0.3% 0%;
     width: 100%;
     background-color: #ededed;
 }
 #sdk-player {
     display: flex;
     flex-direction: column;
-    /* justify-content: center; */
-    /* align-items: center; */
     margin: 0;
 }
 #options {
     display: flex;
     flex-direction: row;
     margin-top: 0.3%;
-    margin-left: 34.9%;
+    margin-left: 41.2%;
 }
 #slide {
-    margin-top: 0.5%;
-    width: 47%;
-    margin-left: 16.9%;
+    margin: 0.5% 0 0 1.3%;
+    width: 70%;
+}
+#volumeAndDevice {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin: 1.4% 0 0 0;
+    padding-left: 5%;
 }
 i {
     font-size: 260%;
     color: rgb(106, 161, 216);
 }
-
+.minorOp {
+    font-size: 150%;
+    margin-top: 1.2%;
+}
+.volumeIcon, .deviceIcon {
+    font-size: 150%;
+}
 i:hover {
     cursor: pointer;
     color: #1b7cde;
