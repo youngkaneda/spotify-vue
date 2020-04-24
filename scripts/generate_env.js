1494a6b99df78fed794e224274bed1178b9d379c
shotaro kaneda <juanpablolvl99@gmail.com>
2020-04-23 22:16:54 -0300

    Fixing env script.

--- scripts/generate_env.js
+++ scripts/generate_env.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 
 fs.writeFileSync(
     './.env',
-    `REDIRECT_URI=${process.env.REDIRECT_URI}\nCLIENT_SECRET=${process.env.CLIENT_SECRET}\nCLIENT_ID=${process.env.process.env.CLIENT_ID}\n`,
+    `REDIRECT_URI=${process.env.REDIRECT_URI}\nCLIENT_SECRET=${process.env.CLIENT_SECRET}\nCLIENT_ID=${process.env.CLIENT_ID}\n`,
 );
