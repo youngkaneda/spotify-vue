b90679976303f1aca350fdb94da3bf7ba16ac451
shotaro kaneda <juanpablolvl99@gmail.com>
2020-04-27 14:42:16 -0300

    Cleaning repo to rebase with new template.

--- scripts/generate_env.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-fs.writeFileSync(
-    './.env',
-    `REDIRECT_URI=${process.env.REDIRECT_URI}\nCLIENT_SECRET=${process.env.CLIENT_SECRET}\nCLIENT_ID=${process.env.CLIENT_ID}\n`,
-);
